10cfc34320e575230b2383a9ad1f0dbb378daa7b
Ryan Thornhill <Ryan@uws08704usr.uws.ualberta.ca>
2013-11-04 12:15:07 -0700

    initial commit

---
+++ Connector/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		MovieDB temp = new MovieDB();
+		temp.createDB();
+		temp.insertMovies();
+		temp.queryAllMovies();
+		temp.close();
+	}
+
+}
