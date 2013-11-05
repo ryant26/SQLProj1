e9c7f4cf0491895eeec46f18ce274315dc3f3e8d
ryant26 <ryant26@gmail.com>
2013-11-04 19:36:17 -0700

    Working on First Part

--- src/Search/Search.java
+++ src/Search/Search.java
@@ -2,6 +2,7 @@ package Search;
 import  Connector.*;
 
 import java.sql.*;
+import java.util.regex.*;
 
 public class Search {
     private DBConnector connector;
@@ -10,5 +11,22 @@ public class Search {
         this.connector = conn;
     }
 
+    public void searchForPatient(String searchString){
+        Pattern p = Pattern.compile("\\d+?");
+        Matcher m = p.matcher(searchString);
+        if (m.find()){
+            //We have a patient #
+        }else{
+            //We have a name
+        }
+    }
+
+    public void searchTestHist(String employee, String sDate, String eDate){
+        //Code Here
+    }
+
+    public void searchAlarmAge(){
+        //Code Here
+    }
 
 }
