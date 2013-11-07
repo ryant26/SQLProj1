081a4b61296b7d7fb909e503b77d8ea70f8595b4
unknown <cbotto@ualberta.ca>
2013-11-06 20:50:35 -0700

    Prescription now works, needs cleaning. Also fixed Scanner in mainInterface

--- src/Prescription/Prescription.java
+++ src/Prescription/Prescription.java
@@ -4,6 +4,7 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.sql.*;
+import Utilities.RPrinter;
 
 import Connector.*;
 
@@ -17,26 +18,27 @@ public class Prescription {
 	public void createPrescription() throws SQLException {
 		int id_of_test = -1;
 		ResultSet test_rs = null;
+		boolean condition = true;
 		Scanner user_input = new Scanner( System.in );
 		user_input.useDelimiter(System.getProperty("line.separator"));
 		
 		System.out.print("Employee Number of Doctor:");
 		String doctor_id = user_input.next();
-		boolean t = true;
-		while(t) {
+		while(condition) {
 		System.out.println("Name of test:");
 		String test_name = user_input.next();
 		
 		test_rs = this.conn.executeQuery("Select t.type_id "
 			+ " From test_type t "
 			+ " Where t.test_name LIKE '"+test_name+"'");
-		
+		RPrinter r = new RPrinter();
+		//r.printResults(test_rs,"type_id");
 		try {
 			if (!test_rs.next()) {
 				System.out.println("Invalid test name, please try again");
 			}
 			else {
-				t = false;
+				condition = false;
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -49,7 +51,6 @@ public class Prescription {
 		System.out.println("Patient health care number:");
 		String patient_id = user_input.next();
 
-		user_input.close();
 		
 		ResultSet rs = this.conn.executeQuery("SELECT UNIQUE n.health_care_no, n.test_id"
 				+ " FROM not_allowed n"
@@ -60,16 +61,17 @@ public class Prescription {
 		try {
 			if (rs.next()) {
 				System.out.println("This combination is not allowed, please try again");
+				condition = true;
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		if (!condition) {
 		Random rand = new Random();
 		
 		while (id_of_test == -1){
-		id_of_test = rand.nextInt(50);
+		id_of_test = rand.nextInt(1000);
 		ResultSet rs2 = this.conn.executeQuery("SELECT test_id from test_record"
 				+ " where test_id = "+id_of_test);
 		if (rs2.next()) {
@@ -87,8 +89,12 @@ public class Prescription {
 								+"	null,null,null )";
 		boolean insert = this.conn.executeNonQuery(sql);
 		System.out.println(""+insert+": Record was stored in database");
+		ResultSet worksmaybe = this.conn.executeQuery("SELECT * FROM test_record");
+		RPrinter r = new RPrinter();
+		r.printResults(worksmaybe, "test_id", "type_id", "patient_no", "employee_no", "medical_lab",
+				"result", "prescribe_date", "test_date");
 		
-
+		}
 		
 	}
 	
